c944782aa769742453fe286a0a962446f3d776b7
pinky <37374201+tko-cactus@users.noreply.github.com>
2024-09-29 19:15:49 +0900

    [remove]remove timer feature (#15)

--- ios/gym-yatteiki/ContentView.swift
+++ ios/gym-yatteiki/ContentView.swift
@@ -4,9 +4,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
-            TimerListView()
+            WorkoutListView()
                 .tabItem {
-                    Label("Timer", systemImage: "timer")
+                    Label("Diary", systemImage: "timer")
                 }
             GraphView()
                 .tabItem {
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,20 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import type { ExportedHandlerFetchHandler } from "@cloudflare/workers-types";
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+export const onRequest: ExportedHandlerFetchHandler = async (
+	request,
+	env,
+	ctx,
+) => {
+	const url = new URL(request.url);
+
+	if (url.pathname === "/hoge") {
+		return new Response("hoge", {
+			headers: { "content-type": "text/plain" },
+		});
+	}
+
+	const response = new Response("Hello World", {
+		headers: { "content-type": "text/plain" },
+	});
+	return response;
+};
